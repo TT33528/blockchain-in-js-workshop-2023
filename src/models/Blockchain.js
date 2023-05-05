050e073a1c7091c425410d625bea5f7892cd0048
TT33528 <132680934+TT33528@users.noreply.github.com>
2023-05-05 23:11:00 +0800

    Update Blockchain.js

--- src/models/Blockchain.js
+++ src/models/Blockchain.js
@@ -7,24 +7,45 @@ class Blockchain {
       - 创世区块
       - 存储区块的映射
   */
-  constructor() {}
+  constructor(name, genesisBlock) {
+    this.name = name;
+    this.blocks = new Map();
+    this.blocks.set(genesisBlock.hash, genesisBlock);
+  }
 
   // 2. 定义 longestChain 函数
   /*
     返回当前链中最长的区块信息列表
   */
   longestChain() {
+    let currentBlock = this.maxHeightBlock();
+    let chain = [];
+    while (currentBlock) {
+      chain.unshift(currentBlock);
+      let previousBlock = this.blocks.get(currentBlock.previousHash);
+      currentBlock = previousBlock;
+    }
     return []
   }
 
   // 判断当前区块链是否包含
   containsBlock(block) {
     // 添加判断方法
-    return false
+    return this.blocks.has(block.hash);
+    //return false
   }
 
   // 获得区块高度最高的区块
   maxHeightBlock() {
+    let maxHeightBlock = null;
+    let maxHeight = -1;
+    for (let block of this.blocks.values()) {
+      if (block.height > maxHeight) {
+        maxHeightBlock = block;
+        maxHeight = block.height;
+      }
+    }
+    return maxHeightBlock;
     // return Block
   }
 
@@ -33,8 +54,25 @@ class Blockchain {
 
   */
   _addBlock(block) {
-    if (!block.isValid()) return
-    if (this.containsBlock(block)) return
+    if (!block.isValid()) return;
+    if (this.containsBlock(block)) return;
+    let parentBlock = this.blocks.get(block.previousHash);
+    if (!parentBlock) return; // 没有找到父区块
+
+    let uPool = new UTXOPool(parentBlock.uPool.getAllUTXO());
+    let isValidTransactions = true;
+    for (let transaction of block.transactions) {
+      if (!transaction.isValid(uPool)) {
+        isValidTransactions = false;
+        break;
+      }
+      transaction.updateUTXOPool(uPool);
+    }
+    if (isValidTransactions) {
+      block.uPool = uPool;
+      block.height = parentBlock.height + 1;
+      this.blocks.set(block.hash, block);
+    }
   }
 }
 
