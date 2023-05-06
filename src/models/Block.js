913a290f118561465e9f05c305a4e9fa6d5047c0
TT33528 <132680934+TT33528@users.noreply.github.com>
2023-05-06 21:29:50 +0800

    Update Block.js

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,43 +1,76 @@
-import sha256 from 'crypto-js/sha256.js'
+import UTXOPool from './UTXOPool.js'
 
-export const DIFFICULTY = 2
-
-class Block {
+class Blockchain {
   // 1. 完成构造函数及其参数
-  constructor(timestamp, transactions, previousHash, nonce = 0) {
-    this.timestamp = timestamp;
-    this.transactions = transactions;
-    this.previousHash = previousHash;
-    this.nonce = nonce;
-    this.hash = this._setHash();
+  /* 构造函数需要包含
+      - 名字
+      - 创世区块
+      - 存储区块的映射
+  */
+  constructor(name, genesisBlock) {
+    this.name = name;
+    this.blocks = new Map();
+    this.blocks.set(genesisBlock.hash, genesisBlock);
   }
 
-  isValid() {}
-
-  setNonce(nonce) {}
-
-  // 根据交易变化更新区块 hash
-  _setHash() {
-    return sha256(this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce).toString();
+  // 2. 定义 longestChain 函数
+  /*
+    返回当前链中最长的区块信息列表
+  */
+  longestChain() {
+    let currentBlock = this.maxHeightBlock();
+    let chain = [];
+    while (currentBlock) {
+      chain.unshift(currentBlock);
+      let previousBlock = this.blocks.get(currentBlock.previousHash);
+      currentBlock = previousBlock;
+    }
+    return chain;
   }
 
-  // 汇总计算交易的 Hash 值
-  /**
-   * 默克尔树实现
-   */
-  combinedTransactionsHash() {}
+  // 判断当前区块链是否包含
+  containsBlock(block) {
+    // 添加判断方法
+    return this.blocks.has(block.hash);
+  }
 
-  // 添加交易到区块
-  /**
-   *
-   * 需包含 UTXOPool 的更新与 hash 的更新
-   */
-  addTransaction() {}
+  // 获得区块高度最高的区块
+  maxHeightBlock() {
+    let maxHeightBlock = null;
+    let maxHeight = -1;
+    for (let block of this.blocks.values()) {
+      if (block.height > maxHeight) {
+        maxHeightBlock = block;
+        maxHeight = block.height;
+      }
+    }
+    return maxHeightBlock;
+  }
 
-  // 添加签名校验逻辑
-  isValidTransaction(transaction) {
+  // 添加区块
+  /*
+  */
+  _addBlock(block) {
+    if (!block.isValid()) return;
+    if (this.containsBlock(block)) return;
+    let parentBlock = this.blocks.get(block.previousHash);
+    if (!parentBlock) return; // 没有找到父区块
 
+    let uPool = new UTXOPool(parentBlock.uPool.getAllUTXO());
+    let isValidTransactions = true;
+    for (let transaction of block.transactions) {
+      if (!transaction.isValid(uPool)) {
+        isValidTransactions = false;
+        break;
+      }
+      transaction.updateUTXOPool(uPool);
+    }
+    if (isValidTransactions) {
+      block.uPool = uPool;
+      block.height = parentBlock.height + 1;
+      this.blocks.set(block.hash, block);
+    }
   }
 }
 
-export default Block
+export default Blockchain
